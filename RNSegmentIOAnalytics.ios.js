9ec0ea235c7e7fa65672a2b25081e06198563b7e
Toby Cox <toby@tobycox.com>
2016-05-27 13:50:51 +0100

    Add alias method

--- RNSegmentIOAnalytics.ios.js
+++ RNSegmentIOAnalytics.ios.js
@@ -80,6 +80,13 @@ var RNSegmentIOAnalytics = {
     enable: function () {
         NativeRNSegmentIOAnalytics.enable();
     },
+
+    /*
+     * https://segment.com/docs/spec/alias/
+     */
+    alias: function (newId) {
+        NativeRNSegmentIOAnalytics.alias(newId);
+    },
 };
 
 module.exports = RNSegmentIOAnalytics;
